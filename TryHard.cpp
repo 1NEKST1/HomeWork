ce4f98ea250d2d708931c4722e4d4b2057595863
1NEKST1 <159796518+1NEKST1@users.noreply.github.com>
2024-02-21 20:57:56 +0300

    Add files via upload
    ДЗ .ТЕМА: ВВЕДЕНИЕ В ЯЗЫК 
    ПРОГРАММИРОВАНИЯ «C++».КРАЙНИЙ СРОК 24.02.2024

---
+++ TryHard.cpp
@@ -0,0 +1,60 @@
+﻿// TryHard.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+    setlocale(LC_ALL, "rus");
+    //№1
+    //int R1, R2, R3;
+    //cin >> R1;
+    //cin >> R2;
+    //cin >> R3;
+    //cout << "R0=" << 1 / R1 + 1 / R2 + 1 / R3;
+
+    //№2
+  /*  const double pi = 3.14;
+    double S, L, R;
+    cout << "Введите длину окружности: ";
+    cin >> L;
+    R = L / (pi *2);
+    S = pi * R * R;
+    cout << "S = " << S;*/
+        
+    //№3
+    /*float v, t, a;
+    cout << "Введите v:";
+    cin >> v;
+    cout << "Введите a:";
+    cin >> a;
+    cout << "Введите t:";
+    cin >> t;
+    cout << "s = " << v * t + (a * t * t) / 2;*/
+
+
+    
+               
+
+        
+
+    
+    
+ 
+    
+    
+    
+
+
+
+
+
+
+
+
+
+}
+
+
+
