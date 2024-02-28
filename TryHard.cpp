116aefd93f7410147cf0372d46d79a80573efb8a
1NEKST1 <159796518+1NEKST1@users.noreply.github.com>
2024-02-28 23:09:27 +0300

    Add files via upload

--- TryHard.cpp
+++ TryHard.cpp
@@ -1,40 +1,97 @@
-﻿// TryHard.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main()
-{ 
+{
     setlocale(LC_ALL, "rus");
-    //№1
-    /*float sec;
-    cout << "Введите время:";
-    cin >> sec;
-    cout << sec / 60 << "минут" << endl;
-    cout << sec / 3600 << "часов";*/
+    //1
+    /*int n, a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, s = 0, s2 = 0, s3 = 0;
+    cout << "Введите шестизначное число:";
+    cin >> n;
+    s3 = n;
+    d = n % 10;
+    n = n / 10;
+    e = n % 10;
+    n = n / 10;
+    f = n % 10;
+    n = n / 10;
+    a = n % 10;
+    n = n / 10;
+    b = n % 10;
+    n = n / 10;
+    c = n % 10;
+    s = a + b + c;
+    s2 = d + e + f;
+    if(s == s2 && s3 < 1000000) cout << "Ваше число счастливое!";
+    else if (s != s2 && s3 < 1000000) cout << "Ваше число не счастливое:(";
+    else cout << "ERROR";*/
+
+    //2
+    /*int a, b, c, d;
+    int n, d1, d2, d3, d4, f, t;
+    cout << "Введите четырёхзначное число:" << endl;
+    cin >> n;
+    if (n > 9999)
+    {
+        cout << "Число не 4-х значное";
+            
+    }
+    else
+    {
+        
+        d4 = n % 10;
+        t = n / 10;
+        d3 = t % 10;
+        t = t / 10;
+        d2 = t % 10;
+        d1 = t / 10;
+        f = d2 * 1000 + d1 * 100 + d4 * 10 + d3;
+        cout << f;
+    }*/
+
+    //3
+    int a, b, c, d, e, f, g;
+    cin >> a >> b >> c >> d >> e >> f >> g;
+    if (a > b && a > c && a > d && a > e && a > f && a > g) cout << a;
+    else if (b > a && b > c && b > d && b > e && b > f && b > g) cout << b;
+    else if (c > a && c > b && c > d && c > e && c > f && c > g) cout << c;
+    else if (d > a && d > c && d > b && d > e && d > f && d > g) cout << d;
+    else if (e > a && e > c && e > d && e > d && e > f && e > g) cout << e;
+    else if (f > a && f > c && f > d && f > e && f > e && f > g) cout << f;
+    else cout << g;
+
+
+
+
+
+
+
+
+
+}
+
     
-    //№2
-   /* double r;
-    int rubley, kopeek;
-    cout << "Введите дробное число:" << endl;
-    cin >> r;
-    rubley = r;
-    kopeek = (r - rubley) * 100;
-    cout << rubley << " рублей " << kopeek << "копеек";*/
-
-    //№3
-  /*  int d, a;
-    cout << "Введите количесвто дней:" << endl;
-    cin >> d;
-    a = d % 7;
-    cout << "Введите количесвто дней:" << endl;
-    cout << d / 7 << " week " << a << " day ";*/
+        
+            
+        
     
+
+
+        
     
 
 
 
+        
+
+
+        
+    
+
+    
+
+
+    
 
 
 
@@ -49,7 +106,10 @@ int main()
 
 
 
-    //№3
+
+
+
+    
 
 
 
@@ -77,7 +137,7 @@ int main()
 
 
 
-}
+
 
 
 
