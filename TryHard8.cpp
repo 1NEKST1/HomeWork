0e40cf759fb1af573c71cd5ca61d86d5e53fb8ef
1NEKST1 <159796518+1NEKST1@users.noreply.github.com>
2024-04-18 22:43:41 +0300

    Add files via upload

---
+++ TryHard8.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#define random(min,max) min + rand() % (max - min + 1)
+using namespace std;
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(NULL));
+    int size;
+    cout << "Введите размер:";
+    cin >> size;
+    int* p = &size;
+    int* array = new int[size];
+    for (int i = 0;i < size;i++) {
+        array[i] = random(1, 9);
+        cout << array[i] << ' ';
+    }
+    cout << endl;
+    cout << p;
+}
+
+
+
+
+
+
+
+
+    
+        
+            
+        
+    
+
+
+        
+    
+
+
+
+        
+
+
+        
+    
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    
+               
+
+        
+
+    
+    
+ 
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
