13a57118ad27f7b12d72a60f1bf3e23b9adee8a9
1NEKST1 <159796518+1NEKST1@users.noreply.github.com>
2024-05-08 17:36:15 +0300

    Update David1.cpp

--- David1.cpp
+++ David1.cpp
@@ -28,86 +28,13 @@ void showArray(int** arr) {
     }
     cout << endl;
 }
-//void initArray(int* arr) {
-//
-//    int size = _msize(arr) / sizeof(arr[0]);
-//    for (int i = 0;i < size;i++) {
-//        arr[i] = random(1, 9);
-//    }
-//
-//}
-////void showArray(int* arr) {
-////
-////    int size = _msize(arr) / sizeof(arr[0]);
-////    for (int i = 0;i < size;i++) {
-////        cout << arr[i] << ' ';
-////    }
-////
-////}
-//
-//void addEndArray(int*& arr, int n) {
-//    int size = _msize(arr) / sizeof(arr[0]);
-//    int* buf = new int[size + 1];
-//    for (int i = 0;i < size;i++) buf[i] = arr[i];
-//    buf[size] = n;
-//    delete[] arr;
-//    arr = buf;
-//}
-//void indexArray(int*& arr, int a, int n) {
-//    int  size = _msize(arr) / sizeof(arr[0]);
-//    int* buf = new int[size + 1];
-//    for (int i = 0;i < a;i++) {
-//        buf[i] = arr[i];
-//    }
-//    buf[a] = n;
-//    for (int i = a+1,j = a;i < size+1;i++,j++) {
-//        buf[i] = arr[j];
-//    }
-//
-//
-//    
-//    
-//    delete[] arr;
-//    arr = buf;
-//}
-//void indexDelArray(int*& arr, int a) {
-//    int  size = _msize(arr) / sizeof(arr[0]);
-//    int* buf = new int[size - 1];
-//    for (int i = 0,j = 0;i < a;i++,j++) {
-//        if (i == a) j++;
-//        buf[i] = arr[j];
-//    }
-//    
-//    delete[] arr;
-//    arr = buf;
-//}
-//void showArray(int* arr) {
-//        int size = _msize(arr) / sizeof(arr[0]);
-//
-//        for (int i = 0;i < size;i++) {
-//            cout << arr[i] << ' ';
-//        }
-//
-//}
-//
-//
+
 int main()
 {
     setlocale(LC_ALL, "rus");
     srand(time(NULL));
 
-    //    int* array = new int[5];
-    //
-    //   /* int** array1 = new int*[3];
-    //    for(int i = 0;i < 3;i++) {
-    //        array1[i] = new int[4];
-    //    }*/
-    //
-    //    initArray(array);
-    //    showArray(array);
-    //    indexDelArray(array, 2);
-    //    cout << endl;
-    //    showArray(array);
+    
     int row = 3;
     int column = 4;
     int** array = new int*[row];
