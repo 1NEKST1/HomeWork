38c2df4557e1439b491e056ce2f1374929276fe0
1NEKST1 <159796518+1NEKST1@users.noreply.github.com>
2024-05-09 19:01:23 +0300

    Update David2.cpp

--- David2.cpp
+++ David2.cpp
@@ -2,36 +2,6 @@
 #define random(min,max) min + rand() % (max - min + 1)
 using namespace std;
 
-// Добавление элемента в конец массива по индексу и вывод ступенчатого массива.
-//void addElement(int**& arr, int rowIndex, int num) {
-//    int size = _msize(arr[rowIndex]) / sizeof(arr[rowIndex][0]);
-//    int* buf = new int[size + 1];
-//    for (int i = 0;i < size;i++) {
-//        buf[i] = arr[rowIndex][i];
-//    }
-//    buf[size] = num;
-//    delete[] arr[rowIndex];
-//    arr[rowIndex] = buf;
-//
-//}
-////Вывод ступенчатого массива.
-//void showArray(int** arr) {
-//    int size1 = _msize(arr) / sizeof(arr[0]);
-//    int size2;
-//    for (int i = 0;i < size1;i++) {
-//        size2 = _msize(arr[i]) / sizeof(arr[0][0]);
-//        for (int j = 0; j < size2; j++) {
-//            cout << arr[i][j] << ' ';
-//            
-//        }
-//        cout << endl;
-//        
-//    }
-//    cout << endl;
-//}
-
-
-
 
 //ДЗ 06.05
 
